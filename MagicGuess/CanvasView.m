ae0e08a6939557364316bebd77afec9f9c466840
Jorge D. Ortiz-Fuentes <jdortiz@gmail.com>
2015-09-17 22:44:23 +0200

    Set the project up.
    Configure the main and only view. Add a subclass of UIView for the  canvas view.

---
+++ MagicGuess/CanvasView.m
@@ -0,0 +1,23 @@
+//
+//  CanvasView.m
+//  MagicGuess
+//
+//  Created by Jorge D. Ortiz Fuentes on 17/9/15.
+//  Copyright (c) 2015 powwau. All rights reserved.
+//
+
+
+#import "CanvasView.h"
+
+
+@implementation CanvasView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
--- MagicGuess/ViewController.m
+++ MagicGuess/ViewController.m
@@ -6,22 +6,24 @@
 //  Copyright (c) 2015 powwau. All rights reserved.
 //
 
+
 #import "ViewController.h"
+#import "CanvasView.h"
+
 
 @interface ViewController ()
 
+@property (weak, nonatomic) IBOutlet CanvasView *canvasView;
+
 @end
+
+
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-}
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
 @end
