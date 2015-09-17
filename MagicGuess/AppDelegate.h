45a68554022714be7c36135d0c48b810e37462a7
Jorge D. Ortiz-Fuentes <jdortiz@gmail.com>
2015-09-17 21:07:47 +0200

    Initial Commit

---
+++ MagicGuess/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MagicGuess
+//
+//  Created by Jorge D. Ortiz Fuentes on 17/9/15.
+//  Copyright (c) 2015 powwau. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MagicGuess/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MagicGuess
+//
+//  Created by Jorge D. Ortiz Fuentes on 17/9/15.
+//  Copyright (c) 2015 powwau. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
